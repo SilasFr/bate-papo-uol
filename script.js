0b8c1bf0e296c956861b5708b708a550305990b8
SilasFr <silas23.fr@gmail.com>
2021-11-11 20:34:24 -0300

    Entrando no servidor

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+let statusOnline = false
+let userName
+
+while(statusOnline){
+    setInterval(statusUsuario, 5000)
+}
+
+function entrarNaSala(){
+    userName = prompt('Digite seu nome')
+    const promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', {name:userName})
+    promise.then(carregarMsg)
+    promise.catch(status400)
+}
+
 
 function carregarMsg(){
+    statusOnline = true
     const promise = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages')
     promise.then(addMsg)
     console.log(promise)
@@ -11,14 +26,40 @@ function addMsg(resposta){
     for(let i=0; i<actualMsg.length; i++){
         let msg = actualMsg[i]
 
-        liMsg.innerHTML += `
-            <li class="mensagem ${msg.type}">
-                <span class="time">${msg.time}</span> 
-                <span class="from">${msg.from}:</span> 
-                <span class="text"${msg.text}</span>
-            </li>`
-    }
-    
+        if(msg.type==="status"){
+            liMsg.innerHTML += `
+                <li class="mensagem ${msg.type}" data-identifier="message">
+                    <span class="time">(${msg.time})</span> 
+                    <span class="from">${msg.from}</span> 
+                    <span class="text">${msg.text}</span>
+                </li>`
+
+        }else if(msg.type==="message"){
+            liMsg.innerHTML += `
+                <li class="mensagem ${msg.type}" data-identifier="message">
+                    <span class="time">(${msg.time})</span> 
+                    <span class="from">${msg.from}:</span> 
+                    <span class="text">${msg.text}</span>
+                </li>`
+
+        }else if(msg.type==="private_message"){
+            liMsg.innerHTML += `
+                <li class="mensagem ${msg.type}" data-identifier="message">
+                    <span class="time">(${msg.time})</span> 
+                    <span class="from">${msg.from} to ${msg.to}:</span> 
+                    <span class="text">${msg.text}</span>
+                </li>`
+        }
+    }    
 }
 
-carregarMsg()
+function status400(){
+    alert("Já existe um usuário online com esse nome")
+    entrarNaSala()
+}
+
+function statusUsuario(){
+    axios.post('https://mock-api.driven.com.br/api/v4/uol/status', {name:userName})
+}
+
+entrarNaSala()
