86b8cb4931157fa13ae563fcaafbef2a7ecd4d30
SilasFr <silas23.fr@gmail.com>
2021-11-16 17:05:27 -0300

    iniciando tela de participantes

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ let statusOnline = undefined
 let userName = undefined
 let actualMsg = undefined
 let feed = undefined
+let loopParticipants = undefined
 let count =0 
 const telaDeEntrada = document.querySelector('.tela-de-entrada')
+const participantsAside = document.querySelector('.participants')
+const opacity = document.querySelector('.opacity')
 telaDeEntrada.classList.remove('hidden')
+requestParticipants()
+
+function asideParticipants(){
+    participantsAside.classList.remove('hidden')
+    opacity.classList.remove('hidden')
+
+}
+function closeParticipants(){
+    participantsAside.classList.add('hidden')
+    opacity.classList.add('hidden')
+}
 
 function entrarNaSala(){
     const inputNome = document.querySelector('.input-usuario')
@@ -34,45 +48,45 @@ function carregarMsg(){
 function addMsg(resposta){
     let liMsg = document.querySelector('.ulMensagens')
     console.log(resposta)
-
+    
     if(resposta.data!==actualMsg || count === 0){
         actualMsg = resposta.data
-
+        
         count = 1
         liMsg.innerHTML = ''
-    
+        
         for(let i=0; i<actualMsg.length; i++){
             let msg = actualMsg[i]
-    
+            
             if(msg.type==="status"){
                 liMsg.innerHTML += `
-                    <li class="mensagem ${msg.type}" data-identifier="message">
-                        <p>
-                            <span class="time">(${msg.time})</span> 
-                            <span class="from">${msg.from}</span> 
-                            <span class="text">${msg.text}</span>
-                        </p>
-                    </li>`
-    
+                <li class="mensagem ${msg.type}" data-identifier="message">
+                <p>
+                <span class="time">(${msg.time})</span> 
+                <span class="from">${msg.from}</span> 
+                <span class="text">${msg.text}</span>
+                </p>
+                </li>`
+                
             }else if(msg.type==="message"){
                 liMsg.innerHTML += `
-                    <li class="mensagem ${msg.type}" data-identifier="message">
-                        <p>
-                            <span class="time">(${msg.time})</span> 
-                            <span class="from">${msg.from}:</span> 
-                            <span class="text">${msg.text}</span>
-                        </p>
-                    </li>`
-    
+                <li class="mensagem ${msg.type}" data-identifier="message">
+                <p>
+                <span class="time">(${msg.time})</span> 
+                <span class="from">${msg.from}:</span> 
+                <span class="text">${msg.text}</span>
+                </p>
+                </li>`
+                
             }else if(msg.type==="private_message" && (userName===msg.to || userName===msg.from)){
                 liMsg.innerHTML += `
-                    <li class="mensagem ${msg.type}" data-identifier="message">
-                        <p>
-                            <span class="time">(${msg.time})</span> 
-                            <span class="from">${msg.from} to ${msg.to}:</span> 
-                            <span class="text">${msg.text}</span>
-                        </p>
-                    </li>`
+                <li class="mensagem ${msg.type}" data-identifier="message">
+                <p>
+                <span class="time">(${msg.time})</span> 
+                <span class="from">${msg.from} to ${msg.to}:</span> 
+                <span class="text">${msg.text}</span>
+                </p>
+                </li>`
             }
         }
         let ultimaMsg = document.querySelectorAll('.mensagem')
@@ -95,32 +109,101 @@ function enviarMsg(){
         }        
         )
         textoMsg.value = ''
-}
-
-let textoMsg = document.querySelector('.text-input')    
-textoMsg.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        // Trigger the button element with a click
-        document.querySelector(".enviar").click();
     }
+    
+    let textoMsg = document.querySelector('.text-input')    
+    textoMsg.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.querySelector(".enviar").click();
+        }
     })
-
-
-function statusUsuario(){
-    const promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/status', {name: `${userName}`})
-    promise.then(statusResposta)
-    promise.catch(alertaDesconectado)
+    
+    
+    function statusUsuario(){
+        const promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/status', {name: `${userName}`})
+        promise.then(statusResposta)
+        promise.catch(alertaDesconectado)
+    }
+    
+    function alertaDesconectado(resposta){
+        alert('Você foi desconectado devido à inatividade, por favor recarregue a página')
+        console.log(resposta)
+        location.reload()
+    }
+    
+    function statusResposta(resposta){
+        clearInterval(statusOnline)
+        statusOnline = setInterval(statusUsuario, 5000)
+        console.log(resposta)
+    }
+    
+    function requestParticipants(){
+        let participants = axios.get('https://mock-api.driven.com.br/api/v4/uol/participants')
+        participants.then(carregarParticipants)
+        participants.catch(status400)
+    
     }
 
-function alertaDesconectado(resposta){
-    alert('Você foi desconectado devido à inatividade, por favor recarregue a página')
-    console.log(resposta)
-    location.reload()
+    function carregarParticipants(resposta){
+        console.log(resposta)
+        let participantsOnline = resposta.data
+        let ulParticipants = document.querySelector('.participants-online')
+        ulParticipants.innerHTML = 
+        `
+        <div class="participant-li" onclick="selectUser(this)">
+        <ion-icon name="people"></ion-icon>
+        <p>Todos</p>
+        <ion-icon name="checkmark" class="checkmark selected"></ion-icon>
+    </div>
+    `
+    for(let i =0; i<participantsOnline.length; i++){
+        let liParticipants = participantsOnline[i]
+
+        ulParticipants.innerHTML += 
+        `
+            <div class="participant-li" onclick="selectUser(this)">
+                <ion-icon name="person"></ion-icon>
+                <p>${liParticipants.name}</p>
+                <ion-icon name="checkmark" class="checkmark hidden"></ion-icon>
+            </div>
+        `
+    }
+
+    clearInterval(loopParticipants)
+    loopParticipants = setInterval(requestParticipants, 10000)
+
 }
 
-function statusResposta(resposta){
-    clearInterval(statusOnline)
-    statusOnline = setInterval(statusUsuario, 5000)
-    console.log(resposta)
+function selectUser(participantSelected){
+    let checkmark = participantSelected.querySelector('.checkmark')
+    console.log(participantSelected)
+
+    let ulContato = document.querySelectorAll('.participant-li')
+    
+    for(let i =0; i<ulContato.length;i++){
+        let x = ulContato[i].lastElementChild.classList.contains('selected')
+        if( x && (ulContato[i].lastChild!==checkmark) ){
+            ulContato[i].lastElementChild.classList.toggle('selected')
+            checkmark.classList.toggle('selected')
+        }        
+    }    
 }
+
+function selectVisibility(visibilitySelected){
+    let checkmark = visibilitySelected.querySelector('.checkmark')
+
+    let ulVisibility = document.querySelectorAll('.visibility')
+    
+    for(let i =0; i<ulContato.length;i++){
+        let x = ulContato[i].lastElementChild.classList.contains('selected')
+        if( x && (ulContato[i].lastChild!==checkmark) ){
+            ulContato[i].lastElementChild.classList.toggle('selected')
+            checkmark.classList.toggle('selected')
+
+        }        
+    }
+    
+}
+
+
